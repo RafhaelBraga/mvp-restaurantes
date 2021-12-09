168a06dbf39780f47aabf9b10cdb7817e51f52f5
Rafhael Braga <rafhaelmbraga@gmail.com>
2021-12-08 21:00:48 -0300

    criação do projeto

---
+++ src/main/java/com/mvp/demo/MvpRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.mvp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpRestaurantesApplication.class, args);
+	}
+
+}
